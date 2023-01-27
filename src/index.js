bc055e5f06caac7d39ebaa2d286455c18860be57
callixmiller <calmill413@gmail.com>
2023-01-27 16:04:57 -0500

    index.js handling tasks, color coding, and deleteable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
+const form = () => document.querySelector('#create-task-form')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form().addEventListener('submit', handleSubmit)
 });
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const toDoText = document.querySelector('#new-task-description').value
+  if (toDoText === "") {
+    alert("Task needs to be filled out")
+    return false
+  }
+  const li = document.createElement('li')
+  
+  const btn = document.createElement('button')
+  
+  const dueDate = document.querySelector('#dueDate').value
+  if (dueDate === "") {
+    alert("Missing a due date")
+    return false
+  }
+
+  const priority = document.querySelector('#prioritySelector').value
+  if (priority === "--") {
+    alert("Priority level needs indicated")
+    return false
+  }
+  
+  const color = e.target.prioritySelector.value
+  
+  btn.addEventListener('click', handleDelete)
+  
+  btn.textContent = 'x'
+  
+  li.textContent = `${toDoText} ${dueDate} `
+  
+  tasks.appendChild(li)
+  
+  li.className = priority
+  
+  li.appendChild(btn)
+
+  li.style.color = color
+  
+  form().reset()
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
